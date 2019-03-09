8c3c06fab9a440aa490853ed6bf1d841c9b537c1
Shahar Horn <shaharhorn1@gmail.com>
2019-03-09 16:26:35 +0200

    acces to CORS fix.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const request = require('request');
 const api = require('./server/routes/api')
 const port = 8000
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/bikeDB', { useNewUrlParser: true })
 const app = express()
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+  next()
+})
 
 app.use('/', api)
 
